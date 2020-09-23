07cde2d1ed4a79280e8af5f7df77f9223adec4ef
Thiti Dev <thiti.mwk.main@gmail.com>
2020-09-24 00:25:02 +0700

    :wrench: [declaration->@types]: Extending the TextInput Interface to support keydown event

---
+++ @types/TextInputMat.d.ts
@@ -0,0 +1,8 @@
+import * as React from "react";
+import 'react-materialize'
+
+declare module "react-materialize"{
+    interface TextInputProps{
+        onKeyDown: (e:KeyboardEvent) => void;
+    }
+}
