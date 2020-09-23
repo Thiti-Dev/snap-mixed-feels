42afe2835d64dcc3a1fe7bb189a9fa08275a1e81
Thiti Dev <thiti.mwk.main@gmail.com>
2020-09-23 22:29:31 +0700

    :package::sparkles::wrench: Installed and initialized react-materialize

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'materialize-css/dist/css/materialize.min.css';
+import {Textarea} from 'react-materialize'
 
+import M from 'materialize-css';  
+M.AutoInit();
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Textarea
+        id="Textarea-12"
+        label="บอกเราสิว่าคุณกำลังรู้สึกอย่างไรอยู่"
+      />
     </div>
   );
 }
