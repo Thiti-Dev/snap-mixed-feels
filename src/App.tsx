687c7a0005c727d48ce2e0db0305b5ab40272861
Thiti Dev <thiti.mwk.main@gmail.com>
2020-09-23 23:42:24 +0700

    :zap: [App]: Wrapped main component with the (context)global provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
-import {Textarea} from 'react-materialize'
 
 import M from 'materialize-css';  
+import MixedSnapFeels from './components/views/MixedSnapFeels';
+import {GlobalProvider} from './contexts/GlobalContext';
 M.AutoInit();
 function App() {
   return (
     <div className="App">
-      <Textarea
-        id="Textarea-12"
-        label="บอกเราสิว่าคุณกำลังรู้สึกอย่างไรอยู่"
-      />
+      <GlobalProvider>
+        <MixedSnapFeels/>
+      </GlobalProvider>
     </div>
   );
 }
