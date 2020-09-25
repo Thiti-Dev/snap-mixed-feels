6b229b4906570a1335f5cf14f6dfd7176ccca5af
Thiti Dev <thiti.mwk.main@gmail.com>
2020-09-25 23:14:46 +0700

    :zap: [@api-wrapped]: Created getQuotesByKeyword

---
+++ src/apis/quote-garden.ts
@@ -0,0 +1,28 @@
+import axios from 'axios'
+
+interface QuotesArray{
+    _id:string;
+    quoteText:string;
+    quiteAuthor:string;
+}
+
+interface Returntype{
+    statusCode:number;
+    mesage:string;
+    totalPages:number;
+    currentPage:number;
+    quote: QuotesArray[];
+}
+
+//
+// ─── UTILS ──────────────────────────────────────────────────────────────────────
+//
+export async function getQuotesByKeyword(keyword:string): Promise<Returntype[] | undefined>  {
+    try {
+        const _response = await axios.get<{quotes: Returntype[]}>(`https://quote-garden.herokuapp.com/api/v2/quotes/${keyword}`)
+        return _response.data.quotes as Returntype[]
+    } catch (error) {
+        console.log(error)
+    }
+}
+// ────────────────────────────────────────────────────────────────────────────────
