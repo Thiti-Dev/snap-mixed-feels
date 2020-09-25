66794df35efc78700f88c024b2549858fec8806a
Thiti Dev <thiti.mwk.main@gmail.com>
2020-09-26 00:00:04 +0700

    :sparkles::zap: Created MixedSnapFeels functional component which wraps the InputField and upcoming component

---
+++ src/components/views/MixedSnapFeels/MixedSnapFeels.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import {Textarea,Row,Col,TextInput} from 'react-materialize'
+import styles from './styles.module.scss'
+
+import GlobalContext from '../../../contexts/GlobalContext'
+import {InputField} from './wrappers'
+
+export default function MixedSnapFeels() {
+  //Context importation
+  const CONTEXT_global = React.useContext(GlobalContext)
+  // ────────────────────────────────────────────────────────────────────────────────
+
+  React.useEffect(()=> {
+      console.log("[MainComponent]: Mounted")
+  })
+
+
+
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.text_area_holder}>
+          <InputField/>
+      </div>
+    </div>
+  )
+}
