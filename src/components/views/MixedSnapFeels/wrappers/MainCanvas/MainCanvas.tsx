fd3d574c7f294dccd7cb51ffc154e32b70b860f5
Thiti Dev <thiti.mwk.main@gmail.com>
2020-09-26 15:52:18 +0700

    :sparkles: Implemented fn(MadeASnapshot) in the ->@component@mainCanvas

--- src/components/views/MixedSnapFeels/wrappers/MainCanvas/MainCanvas.tsx
+++ src/components/views/MixedSnapFeels/wrappers/MainCanvas/MainCanvas.tsx
@@ -13,6 +13,14 @@ import { css } from "@emotion/core";
 import {RingLoader} from "react-spinners";
 // ────────────────────────────────────────────────────────────────────────────────
 
+import html2canvas from 'html2canvas';
+
+//
+// ─── UTIL ───────────────────────────────────────────────────────────────────────
+//
+import { downloadAs } from '../../../../../utils/downloaderAs'
+// ────────────────────────────────────────────────────────────────────────────────
+
 const override_loader_spinner_style = css`
   display: block;
   margin: 0 auto;
@@ -43,7 +51,26 @@ const TextInCanvasHolder = styled.div`
 export default function MainCanvas() {
     //Context importation
     const CONTEXT_global = React.useContext(GlobalContext)
+    const CanvasRef = React.useRef(null);
     
+    function madeASnapshot(){
+        if(!CanvasRef) return
+        //@desc
+        // useCORS needed to be true , if want to rendering the image that isn't came from the host itself
+        // capture on the specified ref that has been gotten by useRef -> [If CanvasRef cannot manage to get itself a reference it will be screenshottig the empty div instead]
+        let canvas = CanvasRef.current as unknown as HTMLCanvasElement // type conversion
+        html2canvas(canvas || document.createElement("div"),{useCORS:true}).then(function(canvas) {
+            // Convert canvas to png
+            /*
+            //@desc => old school way -> [avoiding this because the name of the file that gonna be downloaded cannot set the name]
+                window.location = canvas.toDataURL("image/png")
+                window.location.href = image;
+            */
+            var image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream"); //Convert image to 'octet-stream' -> for a download // -> Later might be using jsPDF
+            downloadAs(image,'myQuote.png') // -> calling utility to use axios and download the octet-stream then setting the name as the given args
+        });
+    }
+
     //destructure
     const {store} = CONTEXT_global
     const {quote_data,is_loading} = store || {};
@@ -53,7 +80,7 @@ export default function MainCanvas() {
     if(!text && !background_url) return null
     return (
         <div>
-            <CanvasHolder bg_url={background_url}>
+            <CanvasHolder ref={CanvasRef} bg_url={background_url}>
                 <TextInCanvasHolder>
                     <p>{text}</p>
                 </TextInCanvasHolder>
@@ -64,6 +91,7 @@ export default function MainCanvas() {
                 node="b"
                 small
                 waves="red"
+                onClick={madeASnapshot}
             >
                 Export as PNG
             </Button>
